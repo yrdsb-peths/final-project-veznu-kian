17f07aa8d1fc8d067b1b69f1dafb4cdccd88cd3c
348588096 <veznup@gmail.com>
2025-05-20 13:46:23 -0400

    Added Ball to Screen

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Ball ball = new Ball();
+        addObject(ball,getWidth()/2,getHeight()/2);
     }
 }
