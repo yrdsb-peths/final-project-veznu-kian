f06ced2f50761f2a3f58fda3ef7d2121962a4af0
348586678 <348586678@gapps.yrdsb.ca>
2025-05-20 14:02:06 -0400

    fixed keybinds

--- BlockOne.java
+++ BlockOne.java
@@ -18,10 +18,10 @@ public class BlockOne extends Actor
     }
     public void act()
     {
-        if(Greenfoot.isKeyDown("up"))
+        if(Greenfoot.isKeyDown("w"))
         {
             setLocation(getX(), getY()+2);
-        } else if (Greenfoot.isKeyDown("down"))
+        } else if (Greenfoot.isKeyDown("s"))
         {
             setLocation(getX(), getY()-2);
         }
