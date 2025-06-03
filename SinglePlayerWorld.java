f92bd6a9f20e80541d845da4ac39f5b47b72f884
348588096 <veznup@gmail.com>
2025-06-03 14:37:48 -0400

    Adding a RandomPerk Class
    -Added Image
    Plan:
    - Create abilities to support in the player to win such as increasing size of player, slowing speed down and etc

--- SinglePlayerWorld.java
+++ SinglePlayerWorld.java
@@ -2,33 +2,27 @@ import greenfoot.*;
 
 
 public class SinglePlayerWorld extends World {
-    
-    //Players counters
     public int player1Score = 0;
     public int player2Score = 0;
     
-    //Labels to display the players score
     private Label player1Label;
     private Label player2Label;
     
     public SinglePlayerWorld() {
         super(600, 400, 1);
         
-        //set background
         setBackground("images/background_soccer.jpg ");
-        
-        //add the ball for single player
         SinglePlayerBall ball = new SinglePlayerBall();
         addObject(ball,getWidth()/2,getHeight()/2);
         
-        //add the player one model
         PersonOne personOne = new PersonOne();
         addObject(personOne, 50, 200);
         
-        //add the computer model
         ComputerPlayer computer = new ComputerPlayer(ball);
         addObject(computer,550,200);
         
+        RandomPerk random = new RandomPerk();
+        addObject(random,getWidth()/2,getHeight()/2);
         // Initialize labels
         player1Label = new Label("Player 1: 0", 30);
         player2Label = new Label("Player 2: 0", 30);
@@ -40,20 +34,17 @@ public class SinglePlayerWorld extends World {
     
     
     public void addScoreToPlayer1() {
-        // add score to player one and update label
         player1Score++;
         player1Label.setValue("Player 1: " + player1Score);
     }
     
     public void addScoreToPlayer2() {
-        //add score to player two and update label
         player2Score++;
         player2Label.setValue("Player 2: " + player2Score);
     }
     
     public void gameOver()
     {
-        //ends the game and transitions to game over screen
         EndWorld gameOver = new EndWorld();
         Greenfoot.setWorld(gameOver);
     }    
