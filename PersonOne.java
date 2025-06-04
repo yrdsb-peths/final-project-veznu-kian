c9b1466784dde319005221319f79ad738665622e
348586678 <348586678@gapps.yrdsb.ca>
2025-06-04 13:47:50 -0400

    Added comments to code

--- PersonOne.java
+++ PersonOne.java
@@ -12,17 +12,25 @@ public class PersonOne extends Actor
      * Act - do whatever the BlockOne wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    //Stores the direction the character is facing
     String facing = "right";
+    
+    // A placeholder image used for animation
     GreenfootImage run = new GreenfootImage("images/Player_run/Run0.png");
+    
+    //Array to store animation frames for idle/running
     GreenfootImage[] idle = new GreenfootImage[6];
+    
+    // Timer to control animation frame speed
     SimpleTimer animationTimer = new SimpleTimer();
     public PersonOne(){
         for(int i = 0; i < idle.length; i++)
         {
+            //Load each frame of the run animation
            idle[i] = new GreenfootImage("images/Player_run/Run" + i + ".png"); 
         }
-        animationTimer.mark();
-        setImage(idle[0]);
+        animationTimer.mark(); //reset timer
+        setImage(idle[0]); // Set the first image initially
         
     }
     
@@ -30,24 +38,29 @@ public class PersonOne extends Actor
     {
         if(Greenfoot.isKeyDown("w"))
         {
+            // Move up
             setLocation(getX(), getY()-10);
             animatePersonOne();
         } else if (Greenfoot.isKeyDown("s"))
         {
+            //Move down
             setLocation(getX(), getY()+10);
             animatePersonOne();
         }
     }
+    //Keeps track of which animation frame is showing
     int imageIndex = 0;
     public void animatePersonOne()
     {
         if(animationTimer.millisElapsed() < 200)
         {
+            //Dont change image if 200ms hasn't passed
             return;
         }
-        animationTimer.mark();
+        animationTimer.mark(); // reset timer
         if (facing.equals("right"))
         {
+            //Cycle through the idle animation frames
             setImage(idle[imageIndex]);
             imageIndex = (imageIndex + 1) % idle.length;
         }
--- PersonTwo.java
+++ PersonTwo.java
@@ -8,18 +8,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PersonTwo extends Actor
 {
+    //Stores the direction the character is facing
     String facing = "left";
+    
+    // A placeholder image used for animation
     GreenfootImage run = new GreenfootImage("images/Player_run/Run0.png");
+    
+    // Array to store animation frames for idle / running
     GreenfootImage[] idle = new GreenfootImage[6];
+    
+    // Timer to control animation frame speed
     SimpleTimer animationTimer = new SimpleTimer();
     public PersonTwo(){
+        
         for(int i = 0; i < idle.length; i++)
         {
+            //Load each frame of the run animation
            idle[i] = new GreenfootImage("images/Player_run/Run" + i + ".png"); 
            idle[i].mirrorHorizontally();
         }
-        animationTimer.mark();
-        setImage(idle[0]);
+        animationTimer.mark(); // reset timer
+        setImage(idle[0]); // Set the first image initially
         
     }
     
@@ -27,25 +36,30 @@ public class PersonTwo extends Actor
     {
         if(Greenfoot.isKeyDown("up"))
         {
+            // move up
             setLocation(getX(), getY()-10);
             animatePersonTwo();
         } else if (Greenfoot.isKeyDown("down"))
         {
+            // move down
             setLocation(getX(), getY()+10);
             animatePersonTwo();
         }
         
     }
+    //Keeps track of which animation frame is showing
     int imageIndex = 0;
     public void animatePersonTwo()
     {
         if(animationTimer.millisElapsed() < 200)
         {
+            // Don't change image if 200ms hasn't passed
             return;
         }
         animationTimer.mark();
         if (facing.equals("left"))
         {
+            // Cycle through the idle animation frames
             setImage(idle[imageIndex]);
             imageIndex = (imageIndex + 1) % idle.length;
         }
