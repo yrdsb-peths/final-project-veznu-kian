3e2402cb8cff0cee94db85a67288e032189d31af
348588096 <veznup@gmail.com>
2025-06-06 14:38:47 -0400

    Create Banana.java

---
+++ Banana.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Banana here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Banana extends Actor
+{
+    /**
+     * Act - do whatever the Banana wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Banana()
+    {
+        setImage("images/banana");
+    }
+    
+    public void act()
+    {
+        checkCollection();
+    }
+    
+    public void checkCollection()
+    {
+        
+    }
+}
